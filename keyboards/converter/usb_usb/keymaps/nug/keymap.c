0c21f1c639ed55cb0c072d7cdf9e1dc634710f63
nug- <nug_@outlook.com>
2021-06-28 15:14:05 +1000

    Updated converter layout

--- keyboards/converter/usb_usb/keymaps/nug/keymap.c
+++ keyboards/converter/usb_usb/keymaps/nug/keymap.c
@@ -80,7 +80,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,    KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_BSPC, KC_INS,  KC_HOME, KC_PGUP, KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, KC_NO, KC_NO, 
     KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          KC_BSLS, KC_DEL,  KC_END,  KC_PGDN, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, KC_NO, KC_NO, 
     TD(TCAP), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_H,    KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_NO,   KC_ENT,                             KC_NO,   KC_NO,   KC_NO,   KC_PCMM, KC_NO, KC_NO, 
-    KC_LSFT,  KC_NO,   KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,    KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          LT_L1DE, RSFT_UA,          KC_UP,            KC_PGDN, KC_P2,   KC_PGUP, KC_PEQL, KC_NO, KC_NO, 
+    KC_LSFT,  KC_NO,   KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,    KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          LT_L1DE, RSFT_UA,          KC_UP,            KC_PGUP, KC_P2,   KC_PGDN, KC_PEQL, KC_NO, KC_NO, 
     KC_LCTL,  KC_LGUI, KC_LALT, LT_L1DE, LT_L1DE,         LT_L1SP,         LT_L1DE, LT_L1DE, LT_L1DE, LT_L1DE, RALT_LA, RGUI_DA, RCTL_RA, KC_LEFT, KC_DOWN, KC_RGHT, KC_P0,            KC_PDOT, KC_PENT, KC_NO, KC_NO),
 
   [_L1] = LAYOUT_all(
