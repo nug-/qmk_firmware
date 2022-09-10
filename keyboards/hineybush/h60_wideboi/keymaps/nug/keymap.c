5c32c4e148a5588db29da3c6897ef6168edf4ba4
nug- <nug_@outlook.com>
2022-09-10 22:45:58 +1000

    Fixed h60-wideboi layout

--- keyboards/hineybush/h60_wideboi/keymaps/nug/keymap.c
+++ keyboards/hineybush/h60_wideboi/keymaps/nug/keymap.c
@@ -42,9 +42,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_L0] = LAYOUT_all(
     KC_INS,  QK_GESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_BSPC, KC_HOME,
     KC_DEL,  KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGUP,
-    KC_PGUP, TD(TCAP), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,                    KC_PDGN,
+    KC_PGUP, TD(TCAP), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,                    KC_PGDN,
     KC_PGDN, KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_UA, LT_L1DE,                   KC_END,
-    KC_LTCL, KC_LCTL,  KC_LGUI, KC_LALT,                   LT_L1SP,                                     RALT_LA, RGUI_DA, RCTL_RA,                   KC_ENT),
+    KC_LCTL, KC_LCTL,  KC_LGUI, KC_LALT,                   LT_L1SP,                                     RALT_LA, RGUI_DA, RCTL_RA,                   KC_ENT),
 
   [_L1] = LAYOUT_all(
     _______, KC_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_DEL,  _______,
@@ -56,9 +56,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_L2] = LAYOUT_all(
     KC_ESC,  KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_BSPC, KC_HOME,
     KC_DEL,  KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGUP,
-    KC_PGUP, _______,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,                    KC_PDGN,
+    KC_PGUP, _______,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,                    KC_PGDN,
     KC_PGDN, KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_UP,   KC_DEL,                    KC_END,
-    KC_LTCL, KC_LCTL,  KC_NO,   KC_LALT,                   KC_SPC,                                      KC_LEFT, KC_DOWN, KC_RGHT,                   KC_ENT),
+    KC_LCTL, KC_LCTL,  KC_NO,   KC_LALT,                   KC_SPC,                                      KC_LEFT, KC_DOWN, KC_RGHT,                   KC_ENT),
 
   [_L3] = LAYOUT_all(
     KC_NO,   QK_BOOT,  BL_TOGG, BL_DEC,  BL_INC,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
