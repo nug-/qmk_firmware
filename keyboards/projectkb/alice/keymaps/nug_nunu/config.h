7bcf5b712a350afbf075a6a06a23d43ba6b69c58
nug- <nug_@outlook.com>
2022-11-26 22:33:17 +1100

    Added pkb alice nunu layout

---
+++ keyboards/projectkb/alice/keymaps/nug_nunu/config.h
@@ -0,0 +1,12 @@
+#pragma once
+
+//#include "../../rev2/config.h"
+
+// place overrides here
+#undef TAPPING_TERM
+#undef TAPPING_TOGGLE
+#undef USB_POLLING_INTERVAL_MS
+
+#define TAPPING_TOGGLE 2
+#define TAPPING_TERM 175
+#define USB_POLLING_INTERVAL_MS 1
