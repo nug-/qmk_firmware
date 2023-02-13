7fada7f84b4c64ce9c8a32bd6294f5e2e35435ed
nug- <nug_@outlook.com>
2023-02-13 12:57:42 +1100

    Update fuyu keymap

--- keyboards/zykrah/fuyu/keymaps/nug/keymap.c
+++ keyboards/zykrah/fuyu/keymaps/nug/keymap.c
@@ -173,15 +173,18 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 
 bool rgb_matrix_indicators_user(void) {
     if (host_keyboard_led_state().caps_lock) {
-        uint8_t b = rgb_matrix_get_val();
-        if (b < CAPS_LOCK_VAL_STEP) {
-            b = CAPS_LOCK_VAL_STEP;
-        } else if (b < (CAPS_LOCK_MAX_BRIGHTNESS - CAPS_LOCK_VAL_STEP)) {
-            b += CAPS_LOCK_VAL_STEP;  // one step more than current brightness
-        } else {
-            b = CAPS_LOCK_MAX_BRIGHTNESS;
-        }
-        rgb_matrix_set_color(CAPS_LOCK_LED_INDEX, b, b, b);  // white, with the adjusted brightness
+//        uint8_t b = rgb_matrix_get_val();
+//        if (b < CAPS_LOCK_VAL_STEP) {
+//            b = CAPS_LOCK_VAL_STEP;
+//        } else if (b < (CAPS_LOCK_MAX_BRIGHTNESS - CAPS_LOCK_VAL_STEP)) {
+//            b += CAPS_LOCK_VAL_STEP;  // one step more than current brightness
+//        } else {
+//            b = CAPS_LOCK_MAX_BRIGHTNESS;
+//        }
+        rgb_matrix_set_color(3, 255, 20, 147);  // white, with the adjusted brightness
+    }
+    if (host_keyboard_led_state().scroll_lock) {
+        rgb_matrix_set_color(1, 255, 140, 0);  // white, with the adjusted brightness
     }
     return false;
 }
