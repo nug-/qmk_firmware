4c3231b6bd74e7900c71dc57a4b7e754c8953cd3
nug- <nug_@outlook.com>
2023-07-02 23:10:06 +1000

    Update wideboi

--- keyboards/hineybush/h60_wideboi/config.h
+++ keyboards/hineybush/h60_wideboi/config.h
@@ -16,7 +16,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#include "config_common.h"
+//#include "config_common.h"
 
 /* USB Device descriptor parameter */
 //#define VENDOR_ID 0x04D8
