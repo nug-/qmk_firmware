b2b69ec07e50fb862447dfaa3d85faf7d3c0e53a
nug- <nug_@outlook.com>
2023-07-17 07:17:25 +1000

    Added Ouro layout

--- compile_qmk_keymaps.sh
+++ compile_qmk_keymaps.sh
@@ -15,6 +15,7 @@ qmk compile -c -j 0 -kb geonworks/frogmini/fms -km nug # frog mini *1
 qmk compile -c -j 0 -kb kapcave/gskt00 -km nug # gskt *1
 qmk compile -c -j 0 -kb idb/idb_60 -km nug # idb60 *1
 qmk compile -c -j 0 -kb hineybush/h60 -km nug # kei/ogre/palmetto/ladybird/bowl60 *5 [unikorn (spares)]
+qmk compile -c -j 0 -kb studiokestra/nue -km nug # ouro *1
 qmk compile -c -j 0 -kb mechlovin/jay60 -km nug # unikorn flex cut *1
 qmk compile -c -j 0 -kb mechlovin/zed60 -km nug # derivative
 qmk compile -c -j 0 -kb noxary/260 -km nug # x60r *1
